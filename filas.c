6cc19c772d58e4e00a1858ec12c47f518c86026c
Paulo Henrique <139723788+KukieLuki@users.noreply.github.com>
2024-10-23 17:42:31 -0300

    Update filas.c

--- filas.c
+++ filas.c
@@ -1,1 +1,11 @@
 #include "filas.h"
+
+void iniciarBancadas(Bancada *bancada){
+    for(int i = 0; i <= QTDMAXSERVENTEBANCADA; i++){
+        bancada->vasilha[i] = NULL;
+    }
+}
+
+void ativarBancada(Bancada *bancada, int identificador);
+
+void desativarBancada(Bancada *bancada, int identificador);
