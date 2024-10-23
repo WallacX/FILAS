5221cd5be770890d97f4498e39abb8481bca4790
camila <soaresmoreiracamila@gmail.com>
2024-10-23 16:10:46 -0300

    defini estrutura Ingrediente e void criaIngrediente

--- filas.h
+++ filas.h
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
+#define NPROBING1
+#define QTDMINING1
+#define QTDMAXING1
 
 typedef struct{
-
+    int grauAceitacao;
+    char nome[15];
 }Ingrediente;
+
+void criaIngrediente(Ingrediente *ingrediente, char *nome);
+
