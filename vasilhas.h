968e279b9e2ea6d40789c4a13337b92d1ef74cc3
Igor Dantas <90714543+igordantas-dev@users.noreply.github.com>
2024-10-23 17:36:35 -0300

    Update vasilhas.h

--- vasilhas.h
+++ vasilhas.h
@@ -1,9 +1,31 @@
+#include <stdio.h>
+
 #define CAPACIDADE_INGREDIENTE 10000 //em gramas
-#define TEMPO_SUBSTITUICAO 3//em minutos
+#define TEMPO_SUBSTITUICAO 3 //em minutos
 
 typedef struct{
-    ingrediente *ingrediente;
-    servente *servente;
-    int quantidade_restante;
+    //Ingrediente *ingrediente;
+    int qtdRestante;
 }Vasilha;
 
+#define QTD_MIN_BANCADAS 1
+#define QTD_MAX_BANCADAS 10
+#define QTD_MIN_SERVENTES 3
+#define QTD_MAX_SERVENTES 6
+
+static int qtdBancadasAtv = 0; //quantidade de bancadas ativas
+
+
+typedef struct{
+    Vasilha *vasilhas[QTD_MAX_SERVENTES];
+    int identificador;
+}Bancada;
+
+
+void iniciarBancada(Bancada *bancada);
+
+void desativarBancada(Bancada *bancada);
+
+void iniciarVasilha(Vasilha *vasilha);
+
+void colocarVasilhaNaBancada(Vasilha *vasilha, Bancada *bancada, int pos);
