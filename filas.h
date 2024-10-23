acace97a5e11abbc9bbd50f85dfe26f094966c89
Paulo Henrique <139723788+KukieLuki@users.noreply.github.com>
2024-10-23 17:41:34 -0300

    Update filas.h

--- filas.h
+++ filas.h
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-#define QTDMINBAN 1
-#define QTDMAXBAN 10
+#define QTDMINBANCADA 2
+#define QTDMAXBANCADA 4
 
-#define QTDMINSERVBAN 3
-#define QTDMAXSERVBAN 6
+#define QTDMINSERVENTEBANCADA 3         
+#define QTDMAXSERVENTEBANCADA 6
 
 int qtdbanatv = 0;
 
 typedef struct{
-    Vasilha vasilha[6];
-    int identificador;
+    Vasilha *vasilha[6];
+    int identificador = 0; 
 }Bancada;
 
-void iniciarBancada(Bancada *bancada);
+void iniciarBancadas(Bancada *bancada);
 
-void desativarBancada(Bancada *bancada);
+void ativarBancada(Bancada *bancada, int identificador);
+
+void desativarBancada(Bancada *bancada, int identificador);
