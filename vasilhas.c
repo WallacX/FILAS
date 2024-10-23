f241b144b88480b1064a140f165b8e3b5be5f09a
Igor Dantas <90714543+igordantas-dev@users.noreply.github.com>
2024-10-23 17:37:27 -0300

    Create vasilhas.c

---
+++ vasilhas.c
@@ -0,0 +1,19 @@
+#include "vasilhas.h"
+#include <stdio.h>
+
+void iniciarVasilha(Vasilha *vasilha){
+    vasilha->qtdRestante = CAPACIDADE_INGREDIENTE;
+    printf("\nvasilha iniciada");
+}
+
+void iniciarBancada(Bancada *bancada){
+    bancada->identificador = ++qtdBancadasAtv;
+    for(int i=0; i<QTD_MAX_SERVENTES; i++){
+        bancada->vasilhas[i] = NULL;
+    }
+}
+
+void colocarVasilhaNaBancada(Vasilha *vasilha, Bancada *bancada, int pos){
+    bancada->vasilhas[pos] = vasilha;
+    printf("\nvasilha colocada.");
+}
