2583a610b230b2ad4e4fd76cebb20976882634d2
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-10-23 17:18:49 -0300

    Create usuario.h

---
+++ usuario.h
@@ -0,0 +1,11 @@
+#define QUANT_USU_MAX 5                   //Quantidade máxima de usuários que chegam por segundo.
+
+typedef struct Usuario{
+    int id;
+    int eVegetariano;
+    Bancada *bancada;
+    struct usuario *prox;
+    //int tempoDeEspera; //Ainda não sei se vai ser necessario.
+}Usuario;
+
+void iniciaUsuario (Usuario *usuario, int id);
