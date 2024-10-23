39c928993dcb3912c5e37cebd944095d835c413c
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-10-23 17:22:09 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -1,8 +1,9 @@
+#include <stdbool.h>
 #define QUANT_USU_MAX 5                   //Quantidade máxima de usuários que chegam por segundo.
 
 typedef struct Usuario{
     int id;
-    int eVegetariano;
+    bo eVegetariano;
     Bancada *bancada;
     struct usuario *prox;
     //int tempoDeEspera; //Ainda não sei se vai ser necessario.
