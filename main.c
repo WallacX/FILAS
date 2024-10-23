111f063c29cce019b273e670125aa667dee869ac
Igor Dantas <90714543+igordantas-dev@users.noreply.github.com>
2024-10-23 17:37:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
+#include "vasilhas.h"
 #include <stdio.h>
-#include "filas.h"
 
 int main(){
-    printf("Olá Mundo!");
+    Vasilha vasilha;
+    Bancada bancada;
+    iniciarVasilha(&vasilha);
+    iniciarBancada(&bancada);
+    colocarVasilhaNaBancada(&vasilha, &bancada, 0);
+    printf("\n\nNa bancada %d, tem %d gramas na vasilha", bancada.identificador, bancada.vasilhas[0]->qtdRestante);
     return 0;
 }
