6f299e53c9910ed76c75320a5808487ec1f6da43
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-10-23 17:42:37 -0300

    Create usuario.c

---
+++ usuario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "usuario.h"
+
+
+void iniciaUsuario (Usuario *usuario, int id){
+    usuario -> id = id;
+
+    int vegetariano = rand() % 101;
+    if (vegetariano > 80){
+        usuario -> eVegetariano = true;
+    }
+    else {
+        usuario -> eVegetariano = false;
+    }
+    
+    usuario -> bancada = NULL;
+    usuario -> prox = NULL;
+
+    return;
+}
