e63c671d38f7138e3244df95da7df5eae454df5c
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-10-30 10:14:17 -0300

    Update usuario.c

--- usuario.c
+++ usuario.c
@@ -1,15 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "usuario.h"
 
-void iniciaUsuario (Usuario *usuario, int id){
-    usuario -> id = ++id; 
 
-    int vegetariano = rand() % 101;
+
+void iniciaUsuario (Usuario *usuario, int id){
+    usuario -> id = id;
+    //define se o usuario será vegetariano ou não.
+    int vegetariano = rand() % 101;            
     if (vegetariano > 80){
         usuario -> eVegetariano = true;
     }
     else {
         usuario -> eVegetariano = false;
     }
+    //define os graus de aceitação do usuario para cada ingrediente.
+    for (int i = 0; i < 6; j++){
+        usuario -> grauAceitacao[i] = rand() % 101;
+    }
+    
+    usuario -> bancada = NULL;                    
 
     return;
 }
