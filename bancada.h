480fe79672a2ce40e400aad6bf2f1f83de2c59cf
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-13 18:01:49 -0300

    Update bancada.h

--- bancada.h
+++ bancada.h
@@ -1,26 +1,34 @@
+#ifndef BANCADA_H
+#define BANCADA_H
+
 #include <stdio.h>
 #include "vasilha.h"
-#include "usuario.h"
+
 
 #define QTDMINBANCADA 2
 #define QTDMAXBANCADA 4
-
-#define QTDMINSERVENTEBANCADA 3         
+#define QTDMINSERVENTEBANCADA 3       
 #define QTDMAXSERVENTEBANCADA 6
 
-int qtdbancadasattvas = 0;
 
-typedef struct{
-    Vasilha *vasilha[6];
-    int identificador; 
-    //int tempoAtendimento; no relatorio precisamos imprimir o tempo medio de atendimento das bancadas
-    //podemos fazer dentro da struct ou ent um vetor global contando que vai somar o tempo dos atendentes
-    Usuario *usuario;
+extern int qtdBancadasAtivas;
+
+
+typedef struct Bancada{
+    int identificador;
     int qtdUsuariosAtendidos;
+    Vasilha vasilhas[TAMCARDAPIO];//trocar pelo tamanho do cardapio
+    //podemos fazer dentro da struct ou ent um vetor global contando que vai somar o tempo dos atendentes
+    //Usuario *usuario;
+    
 }Bancada;
 
-void iniciarBancadas(Bancada *bancada);
 
-void ativarBancada(Bancada *bancada, int identificador);
 
-void desativarBancada(Bancada *bancada, int identificador);
+
+void iniciarBancada(Bancada *bancada, Ingrediente cardapio[]);
+
+
+
+
+#endif // BANCADA_H
