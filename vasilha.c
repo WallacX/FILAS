0afd06ddfdc83f4f78a372b031246364fcd6a293
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-13 18:04:58 -0300

    Update vasilha.c

--- vasilha.c
+++ vasilha.c
@@ -1,18 +1,9 @@
 #include "vasilha.h"
 
-void iniciarVasilha(Vasilha *vasilha){
+
+void iniciarVasilha(Vasilha *vasilha, Ingrediente ingrediente){
+    vasilha->ingrediente = ingrediente; 
     vasilha->qtdRestante = CAPACIDADE_INGREDIENTE;
     printf("\nvasilha iniciada");
 }
 
-void iniciarBancada(Bancada *bancada){
-    bancada->identificador = ++qtdBancadasAtv;
-    for(int i=0; i<QTD_MAX_SERVENTES; i++){
-        bancada->vasilhas[i] = NULL;
-    }
-}
-
-void colocarVasilhaNaBancada(Vasilha *vasilha, Bancada *bancada, int pos){
-    bancada->vasilhas[pos] = vasilha;
-    printf("\nvasilha colocada.");
-}
