e45d4ef673cd480b6a6e97159c094f4f5c18b49b
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-14 11:17:46 -0300

    Update vasilha.c

--- vasilha.c
+++ vasilha.c
@@ -1,7 +1,7 @@
 #include "vasilha.h"
 
 
-void iniciarVasilha(Vasilha *vasilha, Ingrediente ingrediente){
+void iniciaVasilha(Vasilha *vasilha, Ingrediente ingrediente){
     vasilha->ingrediente = ingrediente; 
     vasilha->qtdRestante = CAPACIDADE_INGREDIENTE;
     printf("\nvasilha iniciada");
