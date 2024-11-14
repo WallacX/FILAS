85789bef52e4023484d576761eb7e057dbcd045b
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-14 18:07:57 -0300

    Função de iniciar o usuário

--- usuario.c
+++ usuario.c
@@ -1,19 +1,28 @@
 #include "usuario.h"
 
+int qtdTotalUsuarios = 0;
 
-void iniciaUsuario (Usuario *usuario, int id){
-    usuario -> id = id;
 
-    int vegetariano = rand() % 101;
-    if (vegetariano > 80){
-        usuario -> eVegetariano = true;
+void iniciaUsuario(Usuario *usuario, Ingrediente cardapio[]){
+
+
+    usuario->id = ++qtdTotalUsuarios;
+    //define se o usuario será vegetariano ou não.
+    int veg = 0;
+    veg = rand()%101;
+    printf("veg = %d\n", veg);
+    if(veg > 80)
+        usuario->vegetariano = true;
+    else
+        usuario->vegetariano = false;
+
+    //define os graus de aceitação do usuario para cada ingrediente.
+    int aceita;
+    for(int i = 0; i < 6; i++){
+        aceita = rand() %81 + 20;
+        if(aceita <= cardapio[i].grauAceitacao)
+            usuario->aceitacao[i] = true;
+        else
+            usuario->aceitacao[i] = false;
     }
-    else {
-        usuario -> eVegetariano = false;
-    }
-    
-    usuario -> bancada = NULL;
-    usuario -> prox = NULL;
-
-    return;
 }
