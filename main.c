e66fa496eec83e49e736190f8b76520f04440559
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-14 11:17:23 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// gcc main.c bancada.c vasilha.c ingrediente.c -o programa
+// gcc main.c bancada.c vasilha.c ingrediente.c usuario.c -o programa
 // ./programa
 
 #include <stdio.h>
@@ -10,10 +10,21 @@ int main(){
     Ingrediente cardapio[TAMCARDAPIO];
     criaCardapio(cardapio);
     imprimeCardapio(cardapio);
-    Bancada b1;
-    iniciarBancada(&b1, cardapio);
+    Bancada bancadas[QTDMAXBANCADAS];
+
+    for(int i = 0; i < QTDMAXBANCADAS; i++){
+        iniciaBancada(&bancadas[i], cardapio);
+        
+    }
     
-    printf("B1 %d/%d\n", b1.vasilhas->qtdRestante, b1.vasilhas->ingrediente.grauAceitacao);
-    printf("QTD: %d\n", QTDMAXBANCADA);
+    Usuario u1;
+    iniciaUsuario(&u1, cardapio);
+    printf("\n Usuario ~ id: %d  aceitigd1: %d  aceitigd2: %d  Veg: %s\n", u1.id, u1.aceitacao[0], u1.aceitacao[5], u1.vegetariano ? "true" : "false");
+    
+    printf("\nB1 %d/%d\n", bancadas[0].vasilhas->qtdRestante, bancadas[0].vasilhas[0].ingrediente.grauAceitacao);
+    printf("\nB2 %d/%d\n", bancadas[1].vasilhas->qtdRestante, bancadas[1].vasilhas[1].ingrediente.grauAceitacao);
+    printf("\nB3 %d/%d\n", bancadas[2].vasilhas->qtdRestante, bancadas[2].vasilhas[2].ingrediente.grauAceitacao);
+    printf("\nB4 %d/%d\n", bancadas[3].vasilhas->qtdRestante, bancadas[3].vasilhas[3].ingrediente.grauAceitacao);
+    
     return 0;
 }
