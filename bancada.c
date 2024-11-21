a50b4996e053595b1321881a915bed3f2ab16066
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-21 18:37:50 -0300

    Update bancada.c

--- bancada.c
+++ bancada.c
@@ -7,6 +7,11 @@ void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){
     bancada->identificador = ++qtdBancadasAtivas;
     bancada->qtdUsuariosAtendidos = 0;
     bancada->usuario = NULL;
+    bancada->qtdServenteNaBancada = 0;
+    
+    for(int i = 0; i< TAMCARDAPIO; i++){
+        bancada->idServentes[i] = 0;
+    }
     
     for(int i = 0; i < TAMCARDAPIO; i++)
         iniciaVasilha(&bancada->vasilhas[i], cardapio[i]);
