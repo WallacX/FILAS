819fe8d881350150abe791705cd43617f3489175
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-21 18:38:08 -0300

    Update bancada.c

--- bancada.c
+++ bancada.c
@@ -1,19 +1,31 @@
-#include "bancada.h"
+#ifndef BANCADA_H
+#define BANCADA_H
+
+#include <stdio.h>
+#include "vasilha.h"
 #include "usuario.h"
 
-int qtdBancadasAtivas = 0;
+#define QTDMINBANCADAS 2
+#define QTDMAXBANCADAS 4
+#define QTDMINSERVENTEBANCADA 3       
+#define QTDMAXSERVENTEBANCADA 6
 
-void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){
-    bancada->identificador = ++qtdBancadasAtivas;
-    bancada->qtdUsuariosAtendidos = 0;
-    bancada->usuario = NULL;
-    bancada->qtdServenteNaBancada = 0;
-    
-    for(int i = 0; i< TAMCARDAPIO; i++){
-        bancada->idServentes[i] = 0;
-    }
-    
-    for(int i = 0; i < TAMCARDAPIO; i++)
-        iniciaVasilha(&bancada->vasilhas[i], cardapio[i]);
 
-}
+extern int qtdBancadasAtivas;
+
+
+typedef struct Bancada{
+    int identificador;
+    int qtdUsuariosAtendidos;
+    int qtdServenteNaBancada;
+    Vasilha vasilhas[TAMCARDAPIO];
+    int idServentes[TAMCARDAPIO];
+    //podemos fazer dentro da struct ou ent um vetor global contando que vai somar o tempo dos atendentes
+    Usuario *usuario;
+}Bancada;
+
+
+void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]);
+
+
+#endif // BANCADA_H
