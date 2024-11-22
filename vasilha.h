81702fa50e36dc3585b6bccdff435b158d3c7f1b
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-11-22 08:05:28 -0300

    adicionada a função de checar se quantidade de ingrediente na vasilha é suficiente.

---
+++ vasilha.h
@@ -0,0 +1,21 @@
+#ifndef VASILHA_H
+#define VASILHA_H
+
+#include <stdbool.h>
+#include "ingrediente.h"
+
+#define CAPACIDADE_INGREDIENTE 10000
+#define TEMPO_SUBSTITUICAO 3
+
+
+typedef struct Vasilha{
+    Ingrediente ingrediente;
+    int qtdRestante;
+}Vasilha;
+
+void iniciaVasilha(Vasilha *vasilha, Ingrediente ingrediente);
+
+bool qtdIngredienteSuficiente(Vasilha *vasilha);
+
+
+#endif // VASILHA_H
