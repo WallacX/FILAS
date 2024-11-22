0f756edc1fdf0d8f4a9097c05c4e030d5f98e675
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-22 17:23:25 -0300

    Update bancada.c

--- bancada.c
+++ bancada.c
@@ -1,20 +1,13 @@
 #include "bancada.h"
+#include "usuario.h"
 
 int qtdBancadasAtivas = 0;
 
-// Podemos trocar o nome da função para ativa bancada, e ao inves de um 
-//identificador(dentro da struct) podemos deixar um bool pra saber se a bancada está ativa ou não
-
 void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){
-    bancada->identificador = qtdBancadasAtivas++;  
+    bancada->identificador = ++qtdBancadasAtivas;
     bancada->qtdUsuariosAtendidos = 0;
-    bancada->qtdServentesBancada = 0;
     bancada->usuario = NULL;
 
-    for(int i = 0; i < TAMCARDAPIO; i++){
-        bancada->idServentes[i] = -1;
-    }
-    
     for(int i = 0; i < TAMCARDAPIO; i++)
         iniciaVasilha(&bancada->vasilhas[i], cardapio[i]);
 
