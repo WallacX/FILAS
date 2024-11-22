a6a88b2f82ce69b9e32b960b07ab60dfebbd3780
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-11-22 17:14:29 -0300

    Update main.c

--- main.c
+++ main.c
@@ -19,48 +19,29 @@ int main(){
 
 
     for(int i = 0; i < QTDMAXSERVENTES; i++){
-        iniciaServente(&serventes[i], i);
+        iniciaServente(&serventes[i], i+1);
         printf("Servente: %d T: %d\n", serventes[i].id, serventes[i].tempoAtendimento);
     }
 
     for(int i = 0; i < QTDMAXBANCADAS; i++){
         iniciaBancada(&bancadas[i], cardapio);
-
-        int cont = 0, total;
-        total = rand() % 4 + 3;
-        
-        while(cont != total){
-            posicionaServente(&bancadas[i], serventes);
-            cont++;
-            printf("Rand: %d Contador: %d\n", total, cont);
-        }
-        
-        printf("Bancada %d: ", i);
-        for(int j = 0; j < TAMCARDAPIO; j++){
-            printf("%d ", bancadas[i].idServentes[j]);
-        }
-        printf("\n");
     }
-    
+
     for(int i = 0; i < MAXIMODEFILAS; i++){
         iniciaFila(&filas[i]);
     }
 
-    for(int i = 0; i<QTDMAXSERVENTES; i++){
-        printf("Servente %d: %s\n",i , serventes[i].bancada ? "true" : "false");
-    }
-    
+
     for(int i = 1; i <= 13; i++){
         Usuario *user = (Usuario *)malloc(sizeof(Usuario));
         iniciaUsuario(user, cardapio);
-        printf("Id: %d ~ igd1: %d  igd2: %d igd3: %d igd4: %d igd5: %d igd6: %d  Veg: %s\n", user->id, user->aceitacao[0], user->aceitacao[1], user->aceitacao[2], user->aceitacao[3],user->aceitacao[4], user->aceitacao[5], user->vegetariano ? "true" : "false");
+        printf("Id: %d ~ igd1: %d  igd2: %d igd3: %d igd4: %d igd5: %d igd6: %dVeg: %s\n", user->id, user->aceitacao[0], user->aceitacao[1], user->aceitacao[2], user->aceitacao[3],user->aceitacao[4], user->aceitacao[5], user->vegetariano ? "true" : "false");
         enfileiraUsuario(user, &filas[retornaIndiceMenorFila(filas)]);
     }
 
     for(int i = 0; i < 5; i++){
         printf("Tamanho da Fila %d : %d\n", i+1, filas[i].tamanho);
     }
-    
-    printf("Qtd bancadas ativas: %d\n", qtdBancadasAtivas);
+
     return 0;
 }
