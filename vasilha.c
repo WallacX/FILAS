2bde6db19f2ab396cab75be4db6bdd58b46dc43a
Andressa Gonçalves <andressagb.tech@gmail.com>
2024-11-22 08:21:50 -0300

    Update vasilha.c
    Adicionada função 5

--- vasilha.c
+++ vasilha.c
@@ -1,8 +1,21 @@
 #include "vasilha.h"
+#include <stdio.h>
+#include <stdbool.h>
 
 
 void iniciaVasilha(Vasilha *vasilha, Ingrediente ingrediente){
     vasilha->ingrediente = ingrediente; 
     vasilha->qtdRestante = CAPACIDADE_INGREDIENTE;
     printf("Vasilha iniciada\n");
+}
+
+//Checar se a qtd de ingrediente em cada vasilha é suficiente.
+bool qtdIngredienteSuficiente(Vasilha *vasilha){
+    //Caso a quantidade restante seja igual ou menor a 50 gramas retorna falso.
+    if (vasilha -> qtdRestante <= 50){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
