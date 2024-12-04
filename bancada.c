558b94ad7a0939196a595bfc151f2ebea18597bd
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-12-04 17:43:00 -0300

    Update bancada.c

--- bancada.c
+++ bancada.c
@@ -1,6 +1,6 @@
 #include "bancada.h"
 
-void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){
+void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){//Inicia as variaveis
     bancada->qtdUsuariosAtendidos = 0;
     bancada->qtdServentesBancada = 0;
     bancada->usuario = NULL;
@@ -15,7 +15,7 @@ void iniciaBancada(Bancada *bancada, Ingrediente cardapio[]){
 
 }
 
-void checaVasilhas(Bancada bancadas[]){
+void checaVasilhas(Bancada bancadas[]){//Checa se a quantidade dos ingredientes é suficiente para servir o usuario, caso não seja, preenche a vasilha
     for(int i = 0; i < QTDBANCADAS; i++){
         for(int j = 0; j < TAMCARDAPIO; j++){
             if(bancadas[i].vasilhas[j].qtdRestante < QTDMAXINGREDIENTES[j])
