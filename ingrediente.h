5ced1c289a03cce66931e731a24b4197e13c8659
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-12-04 17:44:08 -0300

    Update ingrediente.h

--- ingrediente.h
+++ ingrediente.h
@@ -1,29 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
 #ifndef INGREDIENTE_H
 #define INGREDIENTE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 #define TAMMAXNOMEINGREDIENTE 15
 #define TAMCARDAPIO 6
 #define TOTALINGREDIENTES 12
 
 
-extern int totalIngredientesConsumidos[6];
-extern const int QTDMAXINGREDIENTES[6];
+extern int totalIngredientesConsumidos[6]; //Vetor que armazena a quantidade total de ingredientes consumidos
+extern const int QTDMAXINGREDIENTES[6]; //Vetores para armazenar a quantidade min e max de cada ingrediente que pode ser consumida por cada usuario
 extern const int QTDMININGREDIENTES[6];
 
 
 typedef struct Ingrediente{
-    int grauAceitacao ;
+    int grauAceitacao; //Armazena a % de aceitação do ingrediente
     char nome[TAMMAXNOMEINGREDIENTE];
 }Ingrediente;
 
-Ingrediente criaIngrediente(int grauAceit, const char nome[]);
-
 void criaCardapio(Ingrediente cardapio[]);
+
+Ingrediente criaIngrediente(int grauAceit, const char nome[]);
 
 void imprimeCardapio(Ingrediente cardapio[]);
 
