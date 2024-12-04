cb1bb85e551600f6610cc3a53558760c1402b5a5
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-12-04 17:46:10 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -9,17 +9,16 @@
 #include "ingrediente.h"
 
 
-#define MAXUSUARIOSGERADOSPORSEGUNDO 2
+#define MAXUSUARIOSGERADOSPORSEGUNDO 2 //Definição da quantidade de usuarios geradas por segundo (-1 por causa do rand)
 
 
-extern int qtdTotalUsuarios;
-extern int qtdUsuariosNasFilas;
+extern int qtdTotalUsuarios; //Variável global que armazena a quantidade total de usuarios gerados
+extern int qtdUsuariosNasFilas; //Variável global que armazena a quantidade atual de usuarios nas filas
 
 typedef struct{
-    int id;
-    bool vegetariano;
-    bool aceitacao[TAMCARDAPIO];
-    int tempo;
+    bool vegetariano; //Váriavel que informa se o usuario é vegetariano ou não
+    bool aceitacao[TAMCARDAPIO]; //Vetor booleano que informa se o usuario aceita ou não o ingrediente
+    int tempo; //Armazena o tempo de espera do usuario na bancada
 }Usuario;
 
 
