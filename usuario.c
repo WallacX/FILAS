2a9ee2eba8eaafae4006b9b6f5592f7b2de4b505
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-12-05 14:02:05 -0300

    Update usuario.c

--- usuario.c
+++ usuario.c
@@ -7,16 +7,17 @@ int qtdUsuariosNasFilas = 0;
 Usuario *geraUsuario(Ingrediente cardapio[]){
     Usuario *usuario = (Usuario *)malloc(sizeof(Usuario));
     
-    int veg = rand()%101; //define se o usuario será vegetariano ou não.
+    usuario->id = ++qtdTotalUsuarios; //Incrementa o contador total de usuarios e atribui ao id do ususario
+    int veg = rand()%101; //Define se o usuario será vegetariano ou não.
     if(veg > 80)
         usuario->vegetariano = true;
     else
         usuario->vegetariano = false;
 
-    //define os graus de aceitação do usuario para cada ingrediente.
+    //Define os graus de aceitação do usuario para cada ingrediente.
     int aceita;
     for(int i = 0; i < 6; i++){
-        aceita = rand() %81 + 20; // Garante que a aceitação do usuario é pelo menos 20 
+        aceita = rand() %81 + 20; //Garante que a aceitação do usuario é pelo menos 20 
         if(aceita <= cardapio[i].grauAceitacao)
             usuario->aceitacao[i] = true;
         else
@@ -25,6 +26,5 @@ Usuario *geraUsuario(Ingrediente cardapio[]){
 
     usuario->tempo = 0;
 
-    qtdTotalUsuarios++;
     return usuario;
 }
