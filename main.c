dc6fd3716f1977ec8a4fc4b8d457829c149d6564
Wallace Elpidio <88462062+WallacX@users.noreply.github.com>
2024-12-06 09:49:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
+// gcc main.c bancada.c vasilha.c ingrediente.c usuario.c fila.c servente.c -o programa
+// ./programa
 
+#include "fila.h"
+
+int main(){
+    srand(time(NULL));
+
+    //Declaração dos vetores com seus respectivos tamanhos
+    Ingrediente cardapio[TAMCARDAPIO];
+    Bancada bancadas[QTDBANCADAS];
+    Fila filas[QTDFILAS];
+    Servente serventes[QTDSERVENTES];
+
+    criaCardapio(cardapio); // Criacao do cardapio
+
+    iniciaRU(cardapio, bancadas, filas, serventes); //Inicialização dos vetores/posicionamento dos serventes na bancada
+
+
+    imprimeCardapio(cardapio);
+    sleep(10);
+    
+    funcionamentoRU(cardapio, bancadas, filas, serventes);
+
+    imprimeRelatorio(cardapio, bancadas, filas, serventes);
+
+   
+
+    return 0;
+}
